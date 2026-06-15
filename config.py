12959bbf6dceeb804a88f18156ddc97fdc77afe2
Vardhu2706 <ayyagari.vardhu@gmail.com>
2026-06-15 18:50:54 +0530

    feat: config

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+DISCOUNT_EVERY_N = int(os.getenv('DISCOUNT_EVERY_N', 5))
+DISCOUNT_PERCENTAGE = float(os.getenv('DISCOUNT_PERCENTAGE', 10.0))
+ADMIN_KEY = os.getenv('ADMIN_KEY', 'secret')
